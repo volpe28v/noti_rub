2863c7dec3a558be21baed7246454f0f28405da6
naoki kodama <naoki.koda@gmail.com>
2013-02-12 07:48:40 +0900

    Refactor a notify logic with module partition.

--- index.js
+++ index.js
@@ -19,8 +19,19 @@ var ExchangeRateNotifier = function(kind){
   this.pre_value = 0;
   //this.url = "http://info.finance.yahoo.co.jp/exchange/convert/?a=1&s=" + this.kind.from + "&t=" + this.kind.to; // 簡易表示
   this.url = "http://stocks.finance.yahoo.co.jp/stocks/detail/?code=" + this.kind.from + this.kind.to + "=X"; // 詳細表示
-};
+}
   
+ExchangeRateNotifier.prototype.startNotify = function(){
+  var that = this;
+  var startTimer = function(){
+    setTimeout(function(){
+      that._scrape(startTimer);
+    }, 30000);
+  };
+
+  that._scrape(startTimer);
+}
+
 ExchangeRateNotifier.prototype._scrape = function(callback){
   var that = this;
   console.log("scrape: " + that.url );
@@ -37,37 +48,33 @@ ExchangeRateNotifier.prototype._scrape = function(callback){
         var $ = window.$;
         //var value = $(".emphasis").text(); // 簡易表示
         var value = $(".stoksPrice").text(); // 詳細表示
-        var nowStr = "[" + moment().format('MM/DD HH:mm:ss') + "]";
-        var msg = nowStr + " " + that.kind.from + "/" + that.kind.to + " : " + value;
-        console.log(msg);
 
-        // 前回値と異なれば Growlへ通知する
-        if ( value != that.pre_value ){
-          var upOrDown = "";
-          if ( value > that.pre_value ){
-            upOrDown = "🔼 ";
-          }else{
-            upOrDown = "🔽 ";
-          }
-
-          // growlの表示のされ方を空白で調整
-          var growl_msg = nowStr + "　　　　　　　　" + upOrDown + that.kind.from + "/" + that.kind.to + " : " + value;
-          growl(growl_msg);
-          that.pre_value = value;
-        }
+        that._notify(value, that.pre_value);
+        that.pre_value = value;
       }
   );
-};
+}
 
-ExchangeRateNotifier.prototype.startNotify = function(){
+ExchangeRateNotifier.prototype._notify = function(value, pre_value){
   var that = this;
-  var startTimer = function(){
-    setTimeout(function(){
-      that._scrape(startTimer);
-    }, 30000);
-  };
 
-  that._scrape(startTimer);
+  var nowStr = "[" + moment().format('MM/DD HH:mm:ss') + "]";
+  var msg = nowStr + " " + that.kind.from + "/" + that.kind.to + " : " + value;
+  console.log(msg);
+
+  // 前回値と異なれば Growlへ通知する
+  if ( value != pre_value ){
+    var upOrDown = "";
+    if ( value > pre_value ){
+      upOrDown = "🔼 ";
+    }else{
+      upOrDown = "🔽 ";
+    }
+
+    // growlの表示のされ方を空白で調整
+    var growl_msg = nowStr + "　　　　　　　　" + upOrDown + that.kind.from + "/" + that.kind.to + " : " + value;
+    growl(growl_msg);
+  }
 }
 
 function main(){
