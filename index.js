b264c464350307eba370a05e05ec1ca4e00af09c
naoki kodama <naoki.koda@gmail.com>
2013-02-11 19:02:35 +0900

    Change url for getting rub

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ var growl = require('growl');
 
 function showRub(){
   jsdom.env(
-    "http://info.finance.yahoo.co.jp/exchange/convert/?a=1&s=RUB&t=JPY",
+//    "http://info.finance.yahoo.co.jp/exchange/convert/?a=1&s=RUB&t=JPY",
+    "http://stocks.finance.yahoo.co.jp/stocks/detail/?code=RUBJPY=X",
     ["http://code.jquery.com/jquery.js"],
     function (errors, window) {
       var $ = window.$;
-      var msg = "[" + moment().format('MM/DD, HH:mm') + "]" + " Rub/JPY: " + $(".emphasis").text();
+      var msg = "[" + moment().format('MM/DD, HH:mm') + "]" + " Rub/JPY: " + $(".stoksPrice").text();
       console.log(msg);
       growl(msg);
     }
